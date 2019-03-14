cc9af84818a50b3a4bd038a483e3fe8970a66a5c
Justin Coutinho <justincoutinho@LIVE.CA>
2019-03-14 16:45:08 -0400

    variables for timer added

--- script/timetrack.js
+++ script/timetrack.js
@@ -9,6 +9,28 @@ window.onload = function pageLoad() {
     //Timer
     let timer = document.getElementById('timer');
 
+    //Time values 
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+
+    function stopWatch() {
+        seconds++;
+
+        if (seconds / 60 === 0) {
+            seconds = 0;
+            minutes++;
+
+            if (minutes / 60 === 0) {
+                minutes = 0;
+                hours++;
+            }
+        }
+
+        timer.innerHTML = hours + ":" + minutes + ":" + seconds;
+    }
+    stopWatch();
+
 
 
 
